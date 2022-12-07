d099c544d942e59c3a33200e6ff22ace716265c3
rondelek <ewelina.nowak103@gmail.com>
2022-12-07 23:35:35 +0100

    added media

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -6,19 +6,20 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import krawczyk from './../assets/krawczyk.jpeg';
 import SearchedArtist from "./SearchedArtist";
+import ClickAwayListener from '@mui/material/ClickAwayListener';
 
 export default function SearchResults(props) {
 
     return (
-    <List   className="searchResults" 
-            sx={{ width: '100%' }}>
-        {props.searchResults.map((artist, index) => {
-            return <SearchedArtist  key={index}
-                                    onAdd={props.onAdd}
-                                    artist={artist} 
-                                    playlistTracks={props.playlistTracks}
-                                    setPlaylistTracks={props.setPlaylistTracks}
-                                    onGetTracks={props.onGetTracks}/>})}    
-    </List>
+      <List   className="searchResults" 
+              sx={{ width: '100%' }}>
+          {props.searchResults.map((artist, index) => {
+              return <SearchedArtist  key={index}
+                                      onAdd={props.onAdd}
+                                      artist={artist} 
+                                      playlistTracks={props.playlistTracks}
+                                      setPlaylistTracks={props.setPlaylistTracks}
+                                      onGetTracks={props.onGetTracks}/>})}    
+      </List>
   );
 }
